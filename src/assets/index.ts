9836b7f243e5763a803a48c142847b9941fcf14b
diamondrarktvo <amada.rktvo@gmail.com>
2025-06-14 11:15:59 +0300

    fix: fix problem compile

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,15 +1,13 @@
 // lotties images
-import EmptyListImage from './images/empty_list.json';
-import ConnectionLost from './images/connection_lost.json';
+import EmptyListImage from "./images/empty_list.json";
+import ConnectionLost from "./images/connection_lost.json";
 
 //png images
-import EnglishIcon from './images/english_icon.png';
-import FrenchIcon from './images/french_icon.png';
-import MoonIcon from './images/moon_icon.png';
-import SunIcon from './images/sun_icon.png';
-import LogoAppFull from './images/logo_app_full.png';
-import LogoAppPicto from './images/logo_app_picto.png';
+import EnglishIcon from "./images/english_icon.png";
+import FrenchIcon from "./images/french_icon.png";
+import MoonIcon from "./images/moon_icon.png";
+import SunIcon from "./images/sun_icon.png";
 
 export { EmptyListImage, ConnectionLost };
 
-export { EnglishIcon, FrenchIcon, MoonIcon, SunIcon, LogoAppFull, LogoAppPicto };
+export { EnglishIcon, FrenchIcon, MoonIcon, SunIcon };
--- src/i18n/locales/locales.ts
+++ src/i18n/locales/locales.ts
@@ -1,25 +1,21 @@
 //FR
-import fr_common from './fr/common.json';
-import fr_setting from './fr/setting.json';
-import fr_home from './fr/home.json';
-import fr_login from './fr/login.json';
+import fr_common from "./fr/common.json";
+import fr_setting from "./fr/setting.json";
+import fr_home from "./fr/home.json";
 
 //EN
-import en_common from './en/common.json';
-import en_home from './en/home.json';
-import en_setting from './en/setting.json';
-import en_login from './en/login.json';
+import en_common from "./en/common.json";
+import en_home from "./en/home.json";
+import en_setting from "./en/setting.json";
 
 export const fr = {
-   common: fr_common,
-   home: fr_home,
-   setting: fr_setting,
-   login: fr_login,
+  common: fr_common,
+  home: fr_home,
+  setting: fr_setting,
 };
 
 export const en = {
-   common: en_common,
-   home: en_home,
-   setting: en_setting,
-   login: en_login,
+  common: en_common,
+  home: en_home,
+  setting: en_setting,
 };
